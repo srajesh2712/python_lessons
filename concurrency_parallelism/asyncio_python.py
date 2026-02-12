b40ea18db10a928041a9b909ae897f35f57c26fe
enstenr <enstenr@gmail.com>
2026-02-12 19:20:44 +0000

    adding

--- concurrency_parallelism/asyncio_python.py
+++ concurrency_parallelism/asyncio_python.py
@@ -7,8 +7,17 @@ async def call_api(name,delay):
     print(f' api {name}responded {delay}')
     return f'{name}_data'
 
-if __name__ == '__main__':
+async def main():
     start = time.perf_counter()
-    call_api('order',2)
-    call_api('user',4)
-    call_api('deliver',6)
+    background_job = asyncio.create_task(call_api('start', 10))
+    results=await asyncio.gather(
+         call_api('order', 2),
+         call_api('user', 4),
+         call_api('deliver', 6),
+    )
+    #result_1 = await background_job
+    result_1 = ''
+    return [results,  result_1]
+if __name__ == '__main__':
+    result = asyncio.run(main())
+    print(result)
